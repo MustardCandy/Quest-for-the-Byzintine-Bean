d32a15a634264d8f8c1c852b3f18c56c441bd232
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-02-04 10:07:20 -0500

    Final Fix

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 function map(){
   for (var i = 0; i < array.length; i++) {
     array[i]
-    for (var i = 0; j < array.length; j++) {
+    for (var j = 0; j < array.length; j++) {
       array[j]
     }
   }
