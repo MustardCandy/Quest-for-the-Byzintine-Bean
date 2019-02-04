c51ec815b3c0f0bcadc1e3294a65b34a20eed9ab
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-02-04 10:05:40 -0500

    Merged 2

--- map.js
+++ map.js
@@ -1,11 +1,7 @@
 function map(){
   for (var i = 0; i < array.length; i++) {
     array[i]
-<<<<<<< HEAD
     for (var i = 0; j < array.length; j++) {
-=======
-    for (var a = 0; j < array.length; j++) {
->>>>>>> 74e21944e7e7725c119dcd6a9d100e2053c0794e
       array[j]
     }
   }
