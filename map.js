ae4963286ee1db71c7647ea7809c439b6539aaa3
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-02-04 10:03:02 -0500

    Test 2

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 function map(){
   for (var i = 0; i < array.length; i++) {
     array[i]
-    for (var j = 0; j < array.length; j++) {
+    for (var i = 0; j < array.length; j++) {
       array[j]
     }
   }
