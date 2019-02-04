3fd1ddd4dde282fd5080153bef35836a3be04aa5
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-02-04 10:22:25 -0500

    update map

--- map.js
+++ map.js
@@ -1,8 +1,11 @@
-function map(){
-  for (var i = 0; i < array.length; i++) {
-    array[i]
-    for (var j = 0; j < array.length; j++) {
-      array[j]
-    }
+function map(number, element, name){
+  var number = number;
+  var gridArray = [];
+   for (var i = 0; i < number; i++) {
+         gridArray[i] = [];
+     for (var j = 0; j < number; j++) {
+       gridArray[i][j] = new element(name, [i,j]);
+     }
   }
+  return gridArray;
 }
