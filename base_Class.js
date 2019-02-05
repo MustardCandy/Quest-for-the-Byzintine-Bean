429d4ae56d8cee97f4633ad0f5aff7868bae780e
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-02-05 10:14:00 -0500

    spacing

--- base_Class.js
+++ base_Class.js
@@ -6,33 +6,43 @@ class Basic{
     this._posY = posY;
     this._damage = damage;
   }
+
   get name(){
     return this._name;
   }
+
   set name(name){
     this._name = name;
   }
+
   get health(){
     return this._health;
   }
+  
   set health(health){
     this._health = health;
   }
+
   get posX(){
     return this._posX;
   }
+
   set posX(posX){
     this._posX = posX;
   }
+
   get posY(){
     return this._posy;
   }
+
   set posX(posY){
     this._posY = posY;
   }
+
   get damage(){
     return this._damage;
   }
+
   set damage(damage){
     this._damage = damage;
   }
