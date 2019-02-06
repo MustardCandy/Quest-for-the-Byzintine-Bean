0e87387c3f928cc871173c66a4e153155862e395
DakotaAyer <47248767+DakotaAyer@users.noreply.github.com>
2019-02-06 09:30:05 -0500

    not sure

---
+++ base_Class.js
@@ -0,0 +1,51 @@
+class Basic {
+  constructor(name,health,damage) {
+    this._name = name;
+    this._health = health;
+    this._damage = damage;
+  }
+
+  toString(){
+    return this._name
+  }
+
+  get name(){
+    return this._name;
+  }
+
+  set name(name){
+    this._name = name;
+  }
+
+  get health(){
+    return this._health;
+  }
+
+  set health(health){
+    this._health = health;
+  }
+
+  get posX(){
+    return this._posX;
+  }
+
+  set posX(posX){
+    this._posX = posX;
+  }
+
+  get posY(){
+    return this._posy;
+  }
+
+  set posX(posY){
+    this._posY = posY;
+  }
+
+  get damage(){
+    return this._damage;
+  }
+
+  set damage(damage){
+    this._damage = damage;
+  }
+}
