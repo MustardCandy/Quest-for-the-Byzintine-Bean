dd913a03c1bf77bb0c909da245097d1c0e12c3ee
DakotaAyer <47248767+DakotaAyer@users.noreply.github.com>
2019-02-07 10:49:00 -0500

    Cleaned up code

--- base_Class.js
+++ base_Class.js
@@ -8,47 +8,37 @@ class Basic {
     this._damage = damage;
     //allows somthing that has damage
   }
-//bellow are getters and setters for the basic class {-}7
+  //bellow are getters and setters for the basic class {-}7
   toString(){
     return this._name
   }
-
   get name(){
     return this._name;
   }
-
   set name(name){
     this._name = name;
   }
-
   get health(){
     return this._health;
   }
-
   set health(health){
     this._health = health;
   }
-
   get posX(){
     return this._posX;
   }
-
   set posX(posX){
     this._posX = posX;
   }
-
   get posY(){
     return this._posy;
   }
-
   set posX(posY){
     this._posY = posY;
   }
-
   get damage(){
     return this._damage;
   }
-
   set damage(damage){
     this._damage = damage;
   }
--- map.js
+++ map.js
@@ -1,20 +1,18 @@
 //this page creats the grid
 
-  function myTimer(){
-    update(grid)
-  }
-  function grid(number, element, name = "#"){
-
-    var returnz = [];
-    for (var i = 0; i < number[0]; i++) {
-      returnz.push([]);
-    for (var j = 0; j < number[1]; j++) {
-      returnz[i].push(new element(name,[i,j]));
-    }
+function myTimer(){
+  update(grid)
+}
+function grid(number, element, name = "#"){
+  var returnz = [];
+  for (var i = 0; i < number[0]; i++) {
+    returnz.push([]);
+      for (var j = 0; j < number[1]; j++) {
+        returnz[i].push(new element(name,[i,j]));
+      }
   }
   return returnz;
-  }
-
+}
 function update(){
   for (var i = 0; i < grid.length; i++) {
     for (var j = 0; j < grid.length; j++) {
@@ -22,7 +20,6 @@ function update(){
       grid[i][j].haveBreed = false;
     }
   }
-
   for (var i = 0; i < grid.length; i++) {
     for (var j = 0; j < grid.length; j++) {
       var gridXY = grid[i][j];
@@ -32,44 +29,36 @@ function update(){
   }
   document.getElementById("blah").innerHTML = draw(grid);
 }
-
-
 function draw(grid){
   //draws the orginal grid full of the default Basic class
   var str = "";
   var kewl = grid[0].length;
   var notkewl = grid.length;
   var top = notkewl;
-    str += "+";
-  for (var i = 0; i < kewl; i++) {
-    top = str += "--";
-}
-  str += "+<BR>";
-  for (var i = 0; i < notkewl; i++) {
-    str += "| ";
-  for (var j = 0; j < kewl; j++) {
-      grid[i][j];
-    str += " " + grid[i][j];
-}
-
-    str += "|<BR>";
-}
-
   str += "+";
   for (var i = 0; i < kewl; i++) {
     top = str += "--";
-}
-
+  }
+  str += "+<BR>";
+  for (var i = 0; i < notkewl; i++) {
+    str += "| ";
+      for (var j = 0; j < kewl; j++) {
+        grid[i][j];
+        str += " " + grid[i][j];
+      }
+    str += "|<BR>";
+  }
+  str += "+";
+  for (var i = 0; i < kewl; i++) {
+    top = str += "--";
+  }
   str += "+";
   return str;
 }
   var grid = grid([50,100],Basic);
-
   document.getElementById("paragraph").innerHTML = draw(grid);
-
   //makes a timer that  upadtes the grid every 300 milisecounds
-  document.getElementById("button").onclick = function(){
+document.getElementById("button").onclick = function(){
   document.getElementById("paragraph").innerHTML = draw(grid);
-myTimer();
-
+  myTimer();
 }
