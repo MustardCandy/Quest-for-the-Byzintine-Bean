3eec85f05ea90134f8b8e219f8425ef810c109dc
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-02-11 09:20:03 -0500

    brokeed walls

--- walls.js
+++ walls.js
@@ -1,20 +1,20 @@
-class Wall {
-  constructor(name, location) {
-    this._name = name;
-    this._location = location;
-  }
-}
-
-//getters
-get location(){
-  return this._location
-}
-
-//setters
-set location(location){
-  this._location = location;
-}
-
-toString(){
-  return this._name
-}
+// class Wall {
+//   constructor(name, location) {
+//     this._name = name;
+//     this._location = location;
+//   }
+// }
+//
+// //getters
+// get location(){
+//   return this._location
+// }
+//
+// setters
+// set location(location){
+//   this._location = location;
+// }
+//
+// toString(){
+//   return this._name
+// }
