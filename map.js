c3973838c95923c628ab90c1dff565237ec688ac
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-02-11 10:47:48 -0500

    plz for the love of god

--- map.js
+++ map.js
@@ -1,80 +1,63 @@
-//this page creats the grid
+//map is the grid that they are currently on
 var map = grid([50,100],Basic);
 
-  function myTimer(){
-    var grid = grid();
-    update(grid)
-  }
+document.getElementById("paragraph").innerHTML = draw(map);
 
-    var returnz = [];
-    for (var i = 0; i < number[0]; i++) {
-      returnz.push([]);
-      for (var j = 0; j < number[1]; j++) {
-        returnz[i].push(new element(name,[i,j]));
-      }
-    }
-    return returnz;
+//grid is the grid the hobbits and nazguls live on
+//needs number, elemnt, and a name
+function grid(number, element, name="#"){
+  var number = number;
+  var gridArray = [];
+   for (var i = 0; i < number[0]; i++) {
+         gridArray[i] = [];
+     for (var j = 0; j < number[1]; j++) {
+       gridArray[i][j] = new element(name, [i,j]);
+     }
   }
-
-  function update(){
-  var grid = grid();
-  for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid.length; j++) {
-      grid[i][j].haveMoved = false;
-      grid[i][j].haveBreed = false;
-    }
-  }
-  for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid.length; j++) {
-      var gridXY = grid[i][j];
-      gridXY.move(grid);
-      gridXY.breed(grid);
-    }
-  }
-  document.getElementById("blah").innerHTML = draw(grid);
+  return gridArray;
 }
 
-  function draw(grid){
-    var grid = grid();
-  //draws the orginal grid full of the default Basic class
-  var str = "";
-  var kewl = grid[0].length;
-  var notkewl = grid.length;
-  var top = notkewl;
-  str += "+";
-  for (var i = 0; i < kewl; i++) {
-    top = str += "--";
-  }
-  str += "+<BR>";
-  for (var i = 0; i < notkewl; i++) {
-    str += "| ";
-      for (var j = 0; j < kewl; j++) {
-        grid[i][j];
-        str += " " + grid[i][j];
+//draw creates the grid using characters and line breaks
+function draw(grid){
+    var borderType = ""; //borderType
+
+    var gridLength = grid.length;
+
+    borderType += "+"; //corner
+
+  for (var i = 0; i < grid.length; i++) {
+      borderType += "~~"; //border
+    }
+
+    borderType += "+"; //corner
+
+    borderType += "<br>"; //break used
+
+    for (i = 0; i < gridLength; i++) {
+      borderType += "|" //border
+
+      for (j = 0; j < gridLength; j++) {
+        borderType += " " + grid[i][j]; //spaces
       }
-    str += "|<BR>";
-  }
-  str += "+";
-  for (var i = 0; i < kewl; i++) {
-    top = str += "--";
-  }
-  str += "+";
-  return str;
+
+      borderType += "|" + "<br>"; //border and break
+    }
+
+    borderType += "+"; //corner
+    for (var i = 0; i < grid.length; i++) {
+        borderType += "~~"; //border
+      }
+
+      borderType += "+"; //corner
+
+      borderType += "<br>"; //break
+
+  return borderType; //return all of the borderType
 }
 
-  function grid(number, element, name="#"){
-    var open = [];
-    for (var i = 0; i < number[0]; i++) {
-      open.push([]);
-      for (var j = 0; j < number[1]; j++) {
-        open[i].push(new element(name,[i,j]));
-      }
-    }
-    return open;
-  }
-
-  //makes a timer that  upadtes the grid every 300 milisecounds
-document.getElementById("button2").onclick = function(){
+//button reset
+//re-draws grid
+document.getElementById("button1").onclick = function(){
   document.getElementById("paragraph").innerHTML = draw(map);
-myTimer();
+  console.log("Worked");
 }
