c2c10361f80ba7c09ce3de89d1910c96ff7a16e2
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-07 10:06:17 -0500

    it is slightly more working

--- Living/Subclasses/randNum.js
+++ Living/Subclasses/randNum.js
@@ -1,10 +1,10 @@
 //atempt at random numbers
 function randNum(grid){
     var roomz = {
-      x : Math.random() *grid.length,
-      y : Math.random() *grid.length,
-      xPos: Math.random() *grid.length/4,
-      yPos: Math.random() *grid.length/4
+      x : Math.random() *grid[0].length,
+      y : Math.random() *grid[1].length,
+      xPos: Math.random() *grid[0].length/4,
+      yPos: Math.random() *grid[1].length/4
     }
     roomz.x = Math.floor(roomz.x);
     roomz.y = Math.floor(roomz.y);
