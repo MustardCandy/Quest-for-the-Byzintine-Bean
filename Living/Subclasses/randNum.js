8ac16c77b53c64ef617fc3dda03f3583a59e483d
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-03-07 09:46:25 -0500

    i feel really dumb

--- Living/Subclasses/randNum.js
+++ Living/Subclasses/randNum.js
@@ -1,14 +1,14 @@
 //atempt at random numbers
 function randNum(grid){
     var roomz = {
-      Xpos: Math.random() *grid.length/4,
-      Ypos: Math.random() *grid.length/4,
-      y : Math.random() *grid.length,
       x : Math.random() *grid.length,
+      y : Math.random() *grid.length,
+      xPos: Math.random() *grid.length/4,
+      yPos: Math.random() *grid.length/4
     }
     roomz.x = Math.floor(roomz.x);
     roomz.y = Math.floor(roomz.y);
-    roomz.Xpos = Math.floor(roomz.Xpos);
-    roomz.Ypos = Math.floor(roomz.Ypos);
+    roomz.xPos = Math.floor(roomz.xPos);
+    roomz.yPos = Math.floor(roomz.yPos);
     return roomz;
 }
