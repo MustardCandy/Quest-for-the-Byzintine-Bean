e42bf67e762b86311ed60b6ab597a932c7c587e8
DakotaAyer <47248767+DakotaAyer@users.noreply.github.com>
2019-03-08 09:30:50 -0500

    Spacing & Comments

--- Living/Subclasses/randNum.js
+++ Living/Subclasses/randNum.js
@@ -4,10 +4,8 @@ function randNum(grid){
     x: Math.random() *grid[0].length/4,
     y: Math.random() *grid[1].length/4,
   }
-
   roomz.xPos = Math.random() *(grid[0].length - roomz.x);
   roomz.yPos = Math.random() *(grid[1].length - roomz.y);
-
   roomz.x = Math.floor(roomz.x);
   roomz.y = Math.floor(roomz.y);
   roomz.xPos = Math.floor(roomz.xPos);
