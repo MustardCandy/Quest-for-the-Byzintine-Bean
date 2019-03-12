998c80aeb9e3e1dc4e66fae36bb66eb0cfcd1acd
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-03-12 10:55:01 -0400

    comments for hallway

--- Living/Subclasses/hallway.js
+++ Living/Subclasses/hallway.js
@@ -1,3 +1,6 @@
-function(grid) {
-  
+//function hallway will create 1 by x room to connect rooms to rooms
+//hallways will need a function that is passed grid
+//make rooms always an odd length so the rooms come out of middle
+function hallway(grid) {
+
 }
