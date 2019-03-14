4cc25a2b953fdbc7546cb3a2de6c0667c90a3735
DakotaAyer <47248767+DakotaAyer@users.noreply.github.com>
2019-03-14 10:07:10 -0400

    just more white spacing

---
+++ Living/Subclasses/hallway.js
@@ -0,0 +1,8 @@
+//function hallway will create 1 by x room to connect rooms to rooms
+//hallways will need a function that is passed grid
+//make rooms always an odd length so the rooms come out of middle
+function hallway(grid) {
+
+}
+
+// https://gamedevelopment.tutsplus.com/tutorials/create-a-procedurally-generated-dungeon-cave-system--gamedev-10099
