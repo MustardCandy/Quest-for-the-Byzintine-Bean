61f48131c3f33a7a571b0076daecebeaf46ca532
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-14 09:23:14 -0400

    only odd numbers genirator

--- Living/Subclasses/randNum.js
+++ Living/Subclasses/randNum.js
@@ -1,4 +1,17 @@
-//random number generator for x, y, xPos, & yPos
+//random odd number generator for x, y, xPos, & yPos
+function randomOddNumber(number){
+  var randInt = Math.random()*number;
+  randInt = Math.floor(randInt);
+  console.log(randInt);
+  var remainder = randInt %2;
+  console.log(remainder);
+  if(remainder == 1){
+    return remainder;
+  }else if(remainder == 0){
+    return remainder + 1;
+  }
+}
+
 function randNum(grid){
   var roomz = {
     x: Math.random() *grid[0].length/4,
