81dab3fdc964c3e1900bb733da251ccfe9e177f9
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-15 10:02:59 -0400

    hi

--- Grid/randNum.js
+++ Grid/randNum.js
@@ -1,24 +1,40 @@
 
-//random odd number generator for x, y, xPos, & yPos
-function randomOddNumber(number){
-  var randInt = Math.random()*number;
-  randInt = Math.floor(randInt);
-  console.log(randInt);
-  var remainder = randInt %2;
-  console.log(remainder);
-  if(remainder == 1){
-    return remainder;
-  }else if(remainder == 0){
-    return null;
+function randomOddNumber(){
+  var randIntX = Math.random()*100;
+  var randIntY = Math.random()*50;
+  roomArrX = [];
+  roomArrY = [];
+  randIntX = Math.floor(randIntX);
+  randIntY = Math.floor(randIntY);
+  var remainderX = randIntX %2;
+  var remainderY = randIntY %2;
+  if(remainderX == 1){
+     randIntX;
+    roomArrX.push(randIntX);
+  }else if(remainderX == 0){
+     randIntX = randIntX + 1;
+    roomArrX.push(randIntX);
   }
+  if(remainderY == 1){
+     randIntY;
+    roomArrY.push(randIntY);
+  }else if(remainderY == 0){
+     randIntY = randIntY + 1;
+    roomArrY.push(randIntY);
+  }
+  console.log(roomArrX);
+  console.log(roomArrY);
+  return roomArr;
 }
 
 
-function randNum(grid){
+//random number generator for x, y, xPos, & yPos
+function randNum(grid,randomOddNumber){
   var roomz = {
-    x: Math.random() *grid[0].length/4,
-    y: Math.random() *grid.length/4
+    x: roomArrX/3
+    y: roomArrY/3
   }
+
   roomz.xPos = Math.random() *(grid[0].length - roomz.x);
   roomz.yPos = Math.random() *(grid.length - roomz.y);
 
@@ -27,6 +43,5 @@ function randNum(grid){
 
   roomz.xPos = Math.floor(roomz.xPos);
   roomz.yPos = Math.floor(roomz.yPos);
-
   return roomz;
 }
