fb658bedcbdfd5f59ebd67cc8246617abaf48900
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-20 10:07:29 -0400

    i hate kade so much

--- Grid/multRooms.js
+++ Grid/multRooms.js
@@ -1,8 +1,11 @@
 document.getElementById("button3").onclick = function(){
   for (var i = 0; i < 101; i++) {
-    var rand = randNum(grid,randomOddNumber);
-    var tst = room(map, rand);
-    drawRoom(map, tst);
+    var oddNumber = randomOddNumber(map);
+    console.log(oddNumber)
+    var rand = randNum(map,oddNumber);
+    console.log(rand);
+    var tst = room(grid, rand);
+    drawRoom(grid, tst);
   }
   document.getElementById("paragraph").innerHTML = draw(map);
 }
