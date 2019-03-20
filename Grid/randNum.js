24f7423cda45cec67dd36163c5745c99a65ad78d
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-19 20:30:11 -0400

    not sure why but all objects had this. before them

--- Grid/randNum.js
+++ Grid/randNum.js
@@ -16,26 +16,18 @@ function randomOddNumber(grid){
     roomArrX = randIntX;
   }else if(remainderX == 0){
      randIntX ++;
-<<<<<<< HEAD
     roomArrX = randIntX;
-=======
-    this.roomArr.x.push(randIntX);
->>>>>>> 965b100d849f83cc277a802a75dcb609c7e8d343
   }
   if(remainderY == 1){
      randIntY;
     roomArrY = randIntY;
   }else if(remainderY == 0){
      randIntY ++;
-<<<<<<< HEAD
     roomArrY = randIntY;
   }
   var roomArr = {
     x: roomArrX,
     y: roomArrY
-=======
-    this.roomArr.y.push(randIntY);
->>>>>>> 965b100d849f83cc277a802a75dcb609c7e8d343
   }
   console.log(roomArr.x)
   console.log(roomArr.y)
@@ -43,19 +35,19 @@ function randomOddNumber(grid){
 }
 
 //random number generator to determine size of rooms
-function randNum(grid, roomArr){
+function randNum(grid,roomArr){
   var roomz = {
-    x: roomArr.x/2,
-    y: roomArr.y/2
+    x: roomArr.x/3,
+    y: roomArr.y/3
 }
 
-  this.roomz.xPos = Math.random() *(grid[0].length - roomz.x);
-  this.roomz.yPos = Math.random() *(grid.length - roomz.y);
+roomz.xPos = Math.random() *(grid[0].length - roomz.x);
+roomz.yPos = Math.random() *(grid.length - roomz.y);
 
-  this.roomz.x = Math.ceil(roomz.x);
-  this.roomz.y = Math.ceil(roomz.y);
+roomz.x = Math.ceil(roomz.x);
+roomz.y = Math.ceil(roomz.y);
 
-  this.roomz.xPos = Math.floor(roomz.xPos);
-  this.roomz.yPos = Math.floor(roomz.yPos);
+roomz.xPos = Math.floor(roomz.xPos);
+roomz.yPos = Math.floor(roomz.yPos);
   return roomz;
 }
