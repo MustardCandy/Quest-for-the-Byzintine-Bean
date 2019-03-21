ba689b3facc815787bba54db604cfc33f0db2648
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-20 21:52:39 -0400

    i need teacher guidance on what to do i think im on the right track thoh

--- Grid/multRooms.js
+++ Grid/multRooms.js
@@ -1,10 +1,19 @@
 document.getElementById("button3").onclick = function(){
+  var ihtw = [];
+  for (var i = 0; i < map.length; i++) {
+    map[i]
+
+    for (var j = 0; j < map.length; j++) {
+      map[i]
+      ihtw.push([i,j])
+    }
+  }
+
   for (var i = 0; i < 101; i++) {
     var oddNumber = randomOddNumber(map);
-  
     var rand = randNum(map,oddNumber);
     console.log(rand);
-    var tst = room(grid, rand);
+    var tst = room(map, rand,ihtw);
     drawRoom(map, tst);
   }
   document.getElementById("paragraph").innerHTML = draw(map);
