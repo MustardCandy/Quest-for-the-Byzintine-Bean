659545d0e0dae098b3c98684ecff1e27961f3148
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-03-27 10:03:21 -0400

    i freaking fixed it

--- Grid/randNum.js
+++ Grid/randNum.js
@@ -1,60 +1,86 @@
-//chooses odd numbers for the random number generator
-function randomOddNumber(grid){
-  var roomArr = {
-    x: roomArrX,
-    y: roomArrY
-  }
-  var randIntX = Math.random()*33;
-  var randIntY = Math.random()*17;
-  var roomArrX;
-  var roomArrY;
-  randIntX = Math.floor(randIntX);
-  randIntY = Math.floor(randIntY);
-  var remainderX = randIntX %2;
-  var remainderY = randIntY %2;
-  if(remainderX == 1){
-     randIntX;
-    roomArrX = randIntX;
-  }else if(remainderX == 0){
-     randIntX ++;
-    roomArrX = randIntX;
-  }
-  if(remainderY == 1){
 
-    randIntY;
-    roomArrY = randIntY;
-  }else if(remainderY == 0){
-    randIntY ++;
 
-     randIntY;
-    roomArrY = randIntY;
-  }else if(remainderY == 0){
-     randIntY ++;
-    roomArrY = randIntY;
-  }
-  var roomArr = {
-    x: roomArrX,
-    y: roomArrY
-  }
+  function randomOddNumber(grid){
 
-    return roomArr;
-}
+    var randIntX = Math.random()*25;
+    var randIntY = Math.random()*13;
+    var randIntPosX = Math.random()*99;
+    var randIntPosY = Math.random()*99;
 
-//random number generator to determine size of rooms
-//roomArr is still return nothing ask mr malorie why
-function randNum(grid,roomArr){
-  var roomz = {
-    x: roomArr.x,
-    y: roomArr.y
+    var roomArrX;
+    var roomArrY;
+    var roomPosX;
+    var roomPosY;
+
+    randIntX = Math.floor(randIntX);
+    randIntY = Math.floor(randIntY);
+    randIntPosX = Math.floor(randIntPosX);
+    randIntPosY = Math.floor(randIntPosY);
+
+    var remainderX = randIntX %2;
+    var remainderY = randIntY %2;
+    var remainderPosX = randIntPosX %2;
+    var remainderPosY = randIntPosY %2;
+
+    //odd number for the leangth
+    if(remainderX == 1){
+       randIntX;
+      roomArrX = randIntX;
+    }else if(remainderX == 0){
+       randIntX ++;
+      roomArrX = randIntX;
+    }
+
+    //odd number for the heigth
+    if(remainderY == 1){
+
+      randIntY;
+      roomArrY = randIntY;
+    }else if(remainderY == 0){
+      randIntY ++;
   }
 
-  roomz.xPos = Math.random() *(grid[0].length - roomz.x);
-  roomz.yPos = Math.random() *(grid.length - roomz.y);
+    //odd number for the x coordinate
+    if(remainderPosX == 1){
+       randIntPosX;
+      roomPosX = randIntPosX;
+    }else if(remainderPosX == 0){
+       randIntPosX ++;
+      roomPosX = randIntPosX;
+    }
 
-  roomz.x = Math.floor(roomz.x);
-  roomz.y = Math.floor(roomz.y);
+    //odd number for the y coordinate
+    if(remainderPosY == 1){
+       randIntPosY;
+      roomPosY = randIntPosY;
+    }else if(remainderPosY == 0){
+       randIntPosY ++;
+      roomPosY = randIntPosY;
+    }
+    var roomArr = {
+      x: roomArrX,
+      y: roomArrY,
+      PosY : roomArrY,
+      PosX : roomArrX
+    }
 
-  roomz.xPos = Math.floor(roomz.xPos);
-  roomz.yPos = Math.floor(roomz.yPos);
-    return roomz;
-}
+  //
+      return roomArr;
+  }
+
+  //random number generator to determine size of rooms
+  //roomArr is still return nothing ask mr malorie why
+  function randNum(grid,roomArr){
+    var roomz = {
+      x: roomArr.x,
+      y: roomArr.y,
+      yPos: roomArr.PosY,
+      xPos: roomArr.PosX
+    }
+    roomz.x = Math.floor(roomz.x);
+    roomz.y = Math.floor(roomz.y);
+
+    roomz.xPos = Math.floor(roomz.xPos);
+    roomz.yPos = Math.floor(roomz.yPos);
+      return roomz;
+  }
