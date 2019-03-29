60775eb3c60ee6401b9dd73947f2386641fca365
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-03-29 10:53:35 -0400

    notes and hallway adds

--- Grid/hallway.js
+++ Grid/hallway.js
@@ -2,9 +2,21 @@
 //hallways will need a function that is passed grid
 //make rooms always an odd length so the rooms come out of middle
 function hallUno(map) {
-
+  var blnkArr = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      blnkArr.push([i,j]);
+    }
+  }
+  return blnkArr;
 }
 
 function hallDos(map) {
-
+  var arrBlnk = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      arrBlnk.push([i,j]);
+    }
+  }
+  return arrBlnk;
 }
