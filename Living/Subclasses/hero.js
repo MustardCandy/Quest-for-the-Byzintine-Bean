4e092ed1391a98ad96e7781891be87308744286d
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-04-01 10:39:23 -0400

    only one who did stuff today

--- Living/Subclasses/hero.js
+++ Living/Subclasses/hero.js
@@ -1,26 +1,26 @@
-//creats the "hero" that players will control
-class Hero extends Basic {
-  constructor(location) {
-    super("X",20,5)
-    this._location = location;
-  }
-}
-
-function stringCheck(map,str) {
-  if(str.includes(" ")){
-    return true;
-  }else{
-    return false;
-  }
-  }
-  //a varible so we can pass a function
-var string = stringCheck(str,map);
-
-  function spawn(map,string){
-  if (string = true){
-
-  }else{spawn(map,string)}
-
-
-
-  }
+// //creats the "hero" that players will control
+// class Hero extends Basic {
+//   constructor(location) {
+//     super("X",20,5)
+//     this._location = location;
+//   }
+// }
+//
+// function stringCheck(map,str) {
+//   if(str.includes(" ")){
+//     return true;
+//   }else{
+//     return false;
+//   }
+//   }
+//   //a varible so we can pass a function
+// var string = stringCheck(str,map);
+//
+//   function spawn(map,string){
+//   if (string = true){
+//
+//   }else{spawn(map,string)}
+//
+//
+//
+//   }
