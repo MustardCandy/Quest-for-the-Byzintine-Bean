ee36bd75f84f8fc44884e233147350f1d9d0114d
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-04-03 09:00:42 -0400

    lets talk crap about dakota in the pushes

--- Grid/multRooms.js
+++ Grid/multRooms.js
@@ -3,9 +3,7 @@ document.getElementById("button3").onclick = function(){
     var oddNumber = randomOddNumber(map);
     var rand = randNum(map,oddNumber);
     var mid = midPoint(rand)
-    console.log(mid);
     var tst = room(grid, rand);
-    console.log(tst)
     drawRoom(map, tst);
   }
   document.getElementById("paragraph").innerHTML = draw(map);
