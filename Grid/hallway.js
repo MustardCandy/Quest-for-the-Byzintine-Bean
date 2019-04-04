b4ac10f392f4d811f00a78363750a1392106647b
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-04-04 10:52:40 -0400

    hallway adds and hard code

--- Grid/hallway.js
+++ Grid/hallway.js
@@ -1,14 +1,24 @@
 //function hallway will create 1 by x room to connect rooms to rooms
 //hallways will need a function that is passed grid
 //make rooms always an odd length so the rooms come out of middle
-function hallUno(map) {
-  for (var i = 0; i < array.length; i++) {
-
+function hallX(map) {
+  var room1 = 3;
+  var room2 = 8;
+  var dif = room2 - room1;
+  if (dif > 0) {
+    room1 ++;
+    dif --;
   }
+  console.log(dif);
 }
 
-function hallDos(map) {
-  for (var i = 0; i < array.length; i++) {
-
+function hallY(map) {
+  var room3 = 23;
+  var room4 = 83;
+  var dif = room4 - room3;
+  if (dif > 0) {
+    room1 ++;
+    dif --;
   }
+  console.log(dif);
 }
