8e32a34c6d34e2205deb14069115007915917e50
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-04-05 11:36:52 -0400

    partaly done

--- Grid/hallway.js
+++ Grid/hallway.js
@@ -2,23 +2,11 @@
 //hallways will need a function that is passed grid
 //make rooms always an odd length so the rooms come out of middle
 function hallX(map) {
-  var room1 = 3;
-  var room2 = 8;
-  var dif = room2 - room1;
-  if (dif > 0) {
-    room1 ++;
-    dif --;
-  }
-  console.log(dif);
+  var room1 =;
+  var room2 =;
 }
 
 function hallY(map) {
-  var room3 = 23;
-  var room4 = 83;
-  var dif = room4 - room3;
-  if (dif > 0) {
-    room1 ++;
-    dif --;
-  }
-  console.log(dif);
+  var room1 = ;
+  var room2 = ;
 }
