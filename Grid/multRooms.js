d9d70127f30bc5e980bfa85f943480afc2da5cb2
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-04-05 11:38:12 -0400

    Almost finsihed the for loop

--- Grid/multRooms.js
+++ Grid/multRooms.js
@@ -1,5 +1,8 @@
 document.getElementById("button3").onclick = function(){
-  for (var i = 0; i < 1; i++) {
+  var room1 = null;
+  var room2 = null;
+
+  for (var i = 0; i < 10; i++) {
     var oddNumber = randomOddNumber(map);
     var rand = randNum(map,oddNumber);
     var mid = midPoint(rand);
@@ -7,6 +10,20 @@ document.getElementById("button3").onclick = function(){
     console.log(rand);
     var tst = room(map,rand);
     drawRoom(map,tst);
+    // if a room actually draws
+    room1 = room2;
+    room2 = {x: rand.xCen, y: rand.yCen};
+    // if room draws & room1 is numbers
+    // make a hall
+    console.log(room1);
+    console.log(room2);
+  for (var i = Math.min(room1.x,room2.x); i <= Math.min(room1.x,room2.x); i++); {
+    hallArr[i];
+    for (var j = Math.max(room1.x,room1.x); j <= Math.max(room1.x,room2.x); j++) {
+     hallArr[j];
+    }
+    return hallArr
+  }
   }
   document.getElementById("paragraph").innerHTML = draw(map);
 }
