5382680c9ae4ebf1133690957333eaa8f42c1d29
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-04-10 10:53:47 -0400

    okay i moved them in to functions

--- Grid/hallway.js
+++ Grid/hallway.js
@@ -1,12 +1,43 @@
-//function hallway will create 1 by x room to connect rooms to rooms
-//hallways will need a function that is passed grid
-//make rooms always an odd length so the rooms come out of middle
-// function hallX(map) {
-//   var room1 =;
-//   var room2 =;
-// }
-//
-// function hallY(map) {
-//   var room1 =;
-//   var room2 =;
-// }
+function hallWayX(){
+  var room1 = null;
+  var room2 = null;
+  var hallArrX = [];
+
+  if(dRoom !== false){
+  room1 = room2;
+  room2 = {x: rand.xCen, y: rand.yCen};
+
+
+  if(room1 !== null){
+    console.log(room1);
+    console.log(room2);
+  // if room draws & room1 is numbers
+  // make a hall
+for (var i = Math.min(room1.x,room2.x); i <= Math.max(room1.x,room2.x); i++); {
+  for (var j = Math.min(room1.x,room2.x); j <= Math.max(room1.x,room2.x); j++) {
+
+   hallArrX.push([i,j])
+   console.log(hallArrX);
+  }
+}
+}
+}
+}
+
+function hallWayY(){
+  var room1 = null;
+  var room2 = null;
+  var hallArrY = [];
+  if(dRoom !== false){
+  room1 = room2;
+  room2 = {x: rand.xCen, y: rand.yCen};
+  if(room1 !== null){
+  for (var i = Math.min(room1.y,room2.y); i <= Math.max(room1.y,room2.y); i++); {
+    for (var j = Math.min(room1.y,room2.y); j <= Math.max(room1.y,room2.y); j++) {
+     hallArrY.push([i,j])
+     console.log(hallArrY);
+    }
+  }
+}
+}
+}
--- Grid/multRooms.js
+++ Grid/multRooms.js
@@ -1,11 +1,7 @@
 document.getElementById("button3").onclick = function(){
 
-  var room1 = null;
-  var room2 = null;
-  var hallArrX = [];
   var hallArrY = [];
   for (var i = 0; i < 100; i++) {
-
 
     var oddNumber = randomOddNumber(map);
     var rand = randNum(map,oddNumber);
@@ -13,32 +9,7 @@ document.getElementById("button3").onclick = function(){
     var tst = room(map,rand);
     var dRoom =  drawRoom(map,tst);
     // if a room actually draws
-    if(dRoom !== false){
-    room1 = room2;
-    room2 = {x: rand.xCen, y: rand.yCen};
 
-
-    if(room1 !== null){
-      console.log(room1);
-      console.log(room2);
-    // if room draws & room1 is numbers
-    // make a hall
-  for (var i = Math.min(room1.x,room2.x); i <= Math.max(room1.x,room2.x); i++); {
-    for (var j = Math.min(room1.x,room2.x); j <= Math.max(room1.x,room2.x); j++) {
-
-     hallArrX.push([i,j])
-     console.log(hallArrX);
-    }
-  }
-
-  for (var i = Math.min(room1.y,room2.y); i <= Math.max(room1.y,room2.y); i++); {
-    for (var j = Math.min(room1.y,room2.y); j <= Math.max(room1.y,room2.y); j++) {
-     hallArrY.push([i,j])
-     console.log(hallArrY);
-    }
-  }
-}
-}
   document.getElementById("paragraph").innerHTML = draw(map);
 }
 }
