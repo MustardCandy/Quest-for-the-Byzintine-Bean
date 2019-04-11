73c7cfa24858292a480d7ff347fd9fef4790f9f0
MustardCandy <47151229+MustardCandy@users.noreply.github.com>
2019-04-10 22:33:08 -0400

    room.js update to master

--- Grid/room.js
+++ Grid/room.js
@@ -1,32 +1,45 @@
 //random number generator for room location on grid
-function room (grid, randNum){
+function room (map,roomz){
+  //makes an empty array
   var retArr = [];
-  for (var i = randNum.yPos; i < randNum.y + randNum.yPos; i++) {
-    for (var j = randNum.xPos; j < randNum.x + randNum.xPos; j++) {
+  //the for loop to randomly creat the rooms
+  for (var i = roomz.yPos; i < roomz.y + roomz.yPos; i++) {
+    for (var j = roomz.xPos; j < roomz.x + roomz.xPos; j++) {
+      //pushes to the arrays
       retArr.push([i,j]);
     }
   }
+  //returns the arrays
   return retArr;
 }
 
-//draws room on map
-function drawRoom (map,room){
+//draws room on grid
+function drawRoom (grid, room){
+  //checks if a room can draw in this location
   for (var i = 0; i < room.length; i++) {
-    if (map[room[i][0]][room[i][1]] == " "){
-      return draw(map);
+    if (map[room[i][0]][room[i][1]] == " ") {
+    //if a room does not draw it returns false
+      return false;
     }
   }
   for (var i = 0; i < room.length; i++) {
     map[room[i][0]][room[i][1]] = " ";
   }
+  console.log("yes");
+  console.log(room);
+  //returns the draw room
   return room;
 }
 
 //button reset
 //re-draws grid
 document.getElementById("button2").onclick = function(){
+  //a var for the randNum function
   var rand = randNum(map);
+  //a var for the room function
   var tst = room(map, rand);
+  //draws the room
   drawRoom(map, tst);
+  //replaces the html element paragraph
   document.getElementById("paragraph").innerHTML = draw(map);
 }
