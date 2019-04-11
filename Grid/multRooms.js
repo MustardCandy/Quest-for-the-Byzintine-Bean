651a22a7146427974ec80750ab6c0ce9ced81821
FuseRush <47150589+FuseRush@users.noreply.github.com>
2019-04-11 09:25:16 -0400

    more spacing

--- Grid/multRooms.js
+++ Grid/multRooms.js
@@ -1,23 +1,32 @@
 document.getElementById("button3").onclick = function(){
 
   var hallArrY = [];
+
   //THE FOR LOOP THAT WILL make the rooms draw
   for (var i = 0; i < 100; i++) {
-//a var for the randomoddnumber function
+
+    //a var for the randomoddnumber function
     var oddNumber = randomOddNumber(map);
+
     //a var for the randNum function
     var rand = randNum(map,oddNumber);
+
     //a var the the midpoint function
     var mid = midPoint(rand);
+
     //a var for the room functon
     var tst = room(map,rand);
+
     //a var for the return of the draw room function
     var dRoom = drawRoom(map,tst);
+
     // if a room actually draws
     var hallWay2X = hallWayX(rand,room,dRoom);
+
     //a var for the hallway X
     var hallWay2Y = hallWayY(rand,room,dRoom);
-//draws over the html text
+
+    //draws over the html text
   document.getElementById("paragraph").innerHTML = draw(map);
 }
 }
